477e0502df9eb18be146e16947b1b9dfae8dcad2
Sauron <634205468@qq.com>
2022-11-16 19:49:49 +0800

    add define

--- OneMapKit/Classes/Core/OMKMapView.h
+++ OneMapKit/Classes/Core/OMKMapView.h
@@ -6,6 +6,24 @@
 //  Copyright © 2022 com.sauronpi. All rights reserved.
 //
 
+#if __has_include(<OneMapKit/OMKAMapView.h>)
+#ifndef __HAS_AMap_FRAMEWORK__
 #import "OMKAMapView.h"
+#define __HAS_AMap_FRAMEWORK__
+#endif
+#endif
+
+#if __has_include(<OneMapKit/OMKBaiduMapView.h>)
+#ifndef __HAS_BaiduMap_FRAMEWORK__
 #import "OMKBaiduMapView.h"
+#define __HAS_BaiduMap_FRAMEWORK__
+#endif
+#endif
+
+#if __has_include(<OneMapKit/OMKTencentMapView.h>)
+#ifndef __HAS_TencentMap_FRAMEWORK__
 #import "OMKTencentMapView.h"
+#define __HAS_TencentMap_FRAMEWORK__
+#endif
+#endif
+
