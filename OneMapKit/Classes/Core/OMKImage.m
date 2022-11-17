dc52ca5534f48c1d20714a062e5adcdd6498d952
Sauron <634205468@qq.com>
2022-11-17 11:43:40 +0800

    fixe bug

--- OneMapKit/Classes/Core/OMKImage.m
+++ OneMapKit/Classes/Core/OMKImage.m
@@ -10,10 +10,12 @@
 @implementation OMKImage
 
 + (UIImage *)bundleImage:(NSString *)imageName {
-    NSBundle *targetBundle = [NSBundle bundleForClass:[self class]];
-    return [UIImage imageNamed:imageName
-                                inBundle:targetBundle
-           compatibleWithTraitCollection:nil];
+    static NSBundle *resourceBundle = nil;
+    if (resourceBundle == nil) {
+        NSString *bundlePath = [[NSBundle bundleForClass:[self class]] pathForResource:@"OneMapKit" ofType:@"bundle"];
+        resourceBundle = [NSBundle bundleWithPath:bundlePath];
+    }
+    return [UIImage imageNamed:imageName inBundle:resourceBundle compatibleWithTraitCollection:nil];
 }
 
 + (UIImage *)imageNamed:(NSString *)name {
